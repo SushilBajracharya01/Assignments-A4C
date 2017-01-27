499bad42d89317817f73ac3b24330d49b58ec76a
Sushil Bajracharya <Bajracharya.sushil01@gmail.com>
2017-01-27 23:02:56 +0545

    Create 4. No.of odd and even.c

---
+++ Assignment IV/Array and Strings/4. No.of odd and even.c
@@ -0,0 +1,50 @@
+/* a program to count number of even and odd entered by the user,
+   find out their sum and display them.*/
+   
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	int arr[10],r,zer=0,eve=0,odd=0,esum=0,osum=0,tsum;
+	
+	//loop to input 10 numbers in array.
+	for(r=0;r<10;r++){
+		printf("Enter number : ");
+		scanf("%d",&arr[r]);
+	}
+	
+	//loop to check odd or even
+	for(r=0;r<10;r++){
+		if(arr[r]==0){
+			zer++;
+			continue;
+		}
+		else{
+		if(arr[r]%2==0){
+			eve++;
+			esum+=arr[r];
+		}
+		else{
+			odd++;
+			osum+=arr[r];
+		}
+		}
+	}
+	tsum=esum+osum;
+	
+	printf("\n ***The entered numbers : ***\n\n\t");
+	
+	//loop to display all the numbers in the array.
+	for(r=0;r<10;r++){
+		printf("%d,",arr[r]);
+	}
+  
+	//Result Display.
+	printf("\n");
+	printf("\nNo. of Even : %d\n",eve);
+	printf("sum of all Even : %d\n",esum);
+	printf("No. of Odd : %d\n",odd);
+	printf("sum of all Odd : %d\n",osum);
+	printf("No. of zero : %d\n",zer);
+	return 0;
+}
