a295c6c33f304026e146fb6024193872a51d28a9
Sushil Bajracharya <Bajracharya.sushil01@gmail.com>
2017-02-01 17:37:38 +0545

    Create 6.1 Concatenate two string using strcat().c

---
+++ Assignment IV/Character array and String/6.1 Concatenate two string using strcat().c
@@ -0,0 +1,17 @@
+// A program to concatenate two string using strcat().
+
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char fir[999],sec[999];
+	
+	printf("\nEnter 1st string : ");
+	gets(fir);
+	printf("Enter 2nd string : ");
+	gets(sec);
+	
+	printf("Combinating the 2 strings we get : %s",strcat(fir,sec));
+	
+	return 0;
+}
