5c0ead26a8cdc58a313edd84bd41a0af1b225310
Sushil Bajracharya <Bajracharya.sushil01@gmail.com>
2017-02-12 15:27:35 +0545

    Create 1.1 No return no argument.c
    a Program in C to input a number and find whether the number is exactly divisible by 5 and 7 or not using functions:
    
        No return no argument

---
+++ Assignment IV/1.1 No return no argument.c
@@ -0,0 +1,29 @@
+// a program to check whether the number is divisible by 5 and 7 with no return no argument
+
+#include <stdio.h>
+
+void div (){
+	printf("It is Divisible by 5 and 7.");
+}
+
+void nodiv(){
+	printf("It is NOT divisible by 5 and 7.");
+}
+
+
+int main(){
+	int num;
+
+  printf("Enter any number : ");
+	scanf("%d",&num);
+	
+	if (num%5==0&&num%7==0){
+		div();
+	}
+	else{
+		nodiv();
+  }
+  
+  return 0;
+
+}
